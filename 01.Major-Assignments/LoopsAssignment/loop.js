4f556e9c97df25e4b5e969b38e5e489322ccf32a
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-05-07 23:16:02 +0530

    loops

--- 01.Major-Assignments/LoopsAssignment/loop.js
+++ 01.Major-Assignments/LoopsAssignment/loop.js
@@ -31,3 +31,29 @@ if(num1 == 1 || num1 == 0){
 //  i 1<->5
 
 console.log(factorial); 
+
+// i + j => 2, 3, 7
+
+// for n = 6, m = 3
+// 1 2 1 2 1 2      i = 1
+// 2 1 2 1 2 1      i = 2
+// 1 2 1 2 1 2      i = 3
+
+// 1 2 3 4 5 6 
+
+
+let n = 6;
+let m = 6;
+
+for(let i = 1; i <= m; i++){
+    for(let j = 1; j <= n; j++){
+        if((i+j) % 2 == 0){
+            process.stdout.write(String(1))
+        }else{
+            process.stdout.write(String(2))
+        }
+    }
+    console.log();
+}
+
+
